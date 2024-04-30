ad722cec598898e4f46a7ee88ef2989b8148745b
AbujafarCh <357jafar@gmail.com>
2024-04-30 20:53:31 +0600

    my list cover image z index changed

--- src/components/MyList/MyLists.jsx
+++ src/components/MyList/MyLists.jsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 const MyLists = () => {
     const myLists = useLoaderData()
     const [newMyLists, setNewMyLists] = useState(myLists)
+    console.log(myLists)
 
     return (
         <div className="mb-12">
@@ -14,7 +15,7 @@ const MyLists = () => {
             </Helmet>
             <div className="h-[400px] relative">
                 <img className="h-full w-full object-cover object-center" src="https://i.ibb.co/YZjth5K/mangrove-forest-in-phang-nga-bay-4k-thailand-b-Wtoa2a-Um-Zqara-Wkp-JRob-Wllr-Wdma2-U.jpg" />
-                <div className="z-20 w-full absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2">
+                <div className="z-10 w-full absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2">
                     <h1 className="font-kaushan w-full text-center sm:text-5xl text-3xl font-bold text-[#e2e2e2]">My Lists</h1>
                 </div>
                 <div className="absolute w-full h-full left-0 bottom-0 bg-[#00000075]"></div>
